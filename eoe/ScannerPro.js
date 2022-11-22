f54419252d0d36d6aedaea4a2cd0aeac5d201aa0
also <nieziyue@hotmail.com>
2022-11-22 19:00:27 +0800

    eoe

--- eoe/ScannerPro.js
+++ eoe/ScannerPro.js
@@ -3,7 +3,10 @@
 [rewrite_local]
 
 
-^https?:\/\/api\.revenuecat\.com\/v\d\/(receipts|subscribers).* url script-response-body https://raw.githubusercontent.com/alsoeoe/Dryinfo/main/eoe/ScannerPro.js
+
+^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/(\$RCAnonymousID\%)?(\w)+$ url script-response-body https://raw.githubusercontent.com/alsoeoe/Dryinfo/main/eoe/ScannerPro.js
+
+^https?:\/\/api\.revenuecat\.com\/v\d\/receipts$ url script-response-body https://raw.githubusercontent.com/alsoeoe/Dryinfo/main/eoe/ScannerPro.js
 
 [mitm]
 
